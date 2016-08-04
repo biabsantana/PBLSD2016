caf325451f0f7e8d9b2b67f5c601c5b5bb49f334
Beatriz de Brito <beatrizbritosantana@gmail.com>
2016-08-04 20:37:40 -0300

    correções

--- Programas de testes/InstrucaoPotencia.asm
+++ Programas de testes/InstrucaoPotencia.asm
@@ -1,15 +1,15 @@
 .module calculo_potencia
 	
 	.pseg
-	li $s0, 2 ;base
-	li $s1, 4 ;potencia
-	li $s2, 1 ;contador
-	move $t0, $s0
+	addi $s0, $zero, 2 ;base
+	addi $s1, $zero, 4 ;potencia
+	addi $s2, $zero, 1 ;contador
+	addi $t0, $zero, $s0
 	
 
 	Loop: beq, $s2, $s1, Endfor
 	mul $t0, $t0, $s0
-	add $s2, $s2, 1
+	addi $s2, $s2, 1
 	j Loop
 	Endfor: sw $t0, resFinal
 	

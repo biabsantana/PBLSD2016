e09d73c8c94675bedfad82032391f03754289789
Beatriz de Brito <beatrizbritosantana@gmail.com>
2016-08-07 21:32:35 -0300

    Compilando

--- Programas de testes/InstrucaoBubbleSort.asm
+++ Programas de testes/InstrucaoBubbleSort.asm
@@ -1,38 +1,41 @@
 .module bubble_sort
 	
 	.pseg
+
 	
-	vetor: ;cria um vetor
+	inicio: sw $t0, vetor       # carrega o endereço base do array em $t0
+   	
+	addi $s1, $zero, 5 #tamanho
+	subi $s2, $s1, 1 #i = tamanho
+	addi $s3, $zero, 0 # j=0
+	addi $t1, $zero, 1 #apenas para comparacao
+	addi $s0, $zero, 0 #auxiliar para troca dos vetores
+	
+	#Loop externo	
+	FirstLoop: 
+	sub $s3, $s3, 1
+	slti $t0, $s2, 1 # i<1?
+	beq $t1, $t0, EndFLopp
+		#Loop interno
+		ScndLopp: slt $t0, $s3, $s2 # j<i?
+		bne $t1, $t0, FirstLoop
+		add $t1, $t0, $s3 # $t1 recebe a posicao da base do vetor + s3 q eh o j
+		sw $s0, 0($t1)# aux recebe o valor do vetor da posicao j
+		sw $s4, 1($t1)
+		add $t1, $zero, $s4 #vetor[j] = vetor[j+1]
+		addi $t1, $t1, 1
+		sw $t1, 0($s0) #vetor[j+1] = aux
+		addi $s3, $zero, 1 #j++
+		j ScndLopp #repete
+	
+	EndFLopp:
+	
+	.dseg
+	
+		vetor: #cria um vetor
 		  .word 5
 		  .word 2
 		  .word 7
 		  .word 1
 		  .word 6 
-	
-	inicio: la   $t0, vetor       ; carrega o endereÃ§o base do array em $t0
-   	
-	addi $s1, $zero, 5 ;tamanho
-	subi $s2, $s1, 1 ;i = tamanho
-	addi $s3, $zero, 0 ; j=0
-	addi $t1, $zero, 1 ;apenas para comparacao
-	addi $aux, $zero, 0 ;auxiliar para troca dos vetores
-	
-	;Loop externo	
-	FirstLoop: 
-	sub $s3, $s3, 1
-	slt $t0, s2, 1 ; i<1?
-	beq $t1, $t0, EndFLopp
-		;Loop interno
-		ScndLopp: slt $t0 $s3, $s2 ; j<i?
-		bne $t1, $t0 FirstLoop
-		add $t1, $t0, $s3 ; $t1 recebe a posicao da base do vetor + s3 q eh o j
-		sw $aux, 0($t1); aux recebe o valor do vetor da posicao j
-		add 0($t1), $zero, 1($t1) ;vetor[j] = vetor[j+1]
-		sw 1($t1), $aux ;vetor[j+1] = aux
-		addi $s3, $zero, 1 ;j++
-		j ScndLopp ;repete
-	
-	EndFLopp
-	
-
 .end
--- Programas de testes/InstrucaoRaiz.asm
+++ Programas de testes/InstrucaoRaiz.asm
@@ -1,44 +1,41 @@
 .module calculo_raiz
 	
 	.pseg
-	addi $s0, $zero, 16 ;raiz
-	addi $s1, $zero, 1; ;so para comparacao
-	addi $s2, $zero, 0 ;contador
-	addi $t0, $zero, 0 ;resultado parcial
-	addi $t1, $zero, 0 ;verificador
+	addi $s0, $zero, 16 #raiz
+	addi $s1, $zero, 1 #so para comparacao
+	addi $s2, $zero, 0 #contador
+	addi $t0, $zero, 0 #resultado parcial
+	addi $t1, $zero, 0 #verificador
 
 	
-	Loop: slti $t1, $s0, 1 ;verifica se o resultado parcial é menor do que 1
-	beq $t1, $s1 Endfor ;caso seja menor do que 1 vai pro final do loop
-	mul $t0, $s2, 2 ;indice * 2 + 1
+	Loop: slti $t1, $s0, 1 #verifica se o resultado parcial é menor do que 1
+	beq $t1, $s1 Endfor #caso seja menor do que 1 vai pro final do loop
+	mul $t0, $s2, 2 #indice * 2 + 1
 	add $t0, $t0, 1
-	sub $s0, $s0, $t0 ;raiz - indice
-	add $s2, $s2, 1 ;adiciona 1 ao contador
+	sub $s0, $s0, $t0 #raiz - indice
+	add $s2, $s2, 1 #adiciona 1 ao contador
 	j Loop
-	Endfor: sw resFinal, 0($sp)
+	Endfor: sw $s2, 0($sp)
 	addi $sp, $sp, 1
-	
-	.dseg
-	resFinal: 
-			.word 0
+
 .end
 
 
-;LÓGICA
-; Fonte: http://www.guj.com.br/t/ajuda-em-assembly/283268
-;raiz de 16 = ?
-;1º Passo 16 − 1 = 15
-;2º Passo 15 − 3 = 12
-;3º Passo 12 − 5 = 7
-;4º Passo 7 − 7 = 0
+#L�GICA
+# Fonte: http://www.guj.com.br/t/ajuda-em-assembly/283268
+#raiz de 16 = ?
+#1º Passo 16 − 1 = 15
+#2º Passo 15 − 3 = 12
+#3º Passo 12 − 5 = 7
+#4º Passo 7 − 7 = 0
 
-;O resultado da raiz quadrada é o contador => 4
+#O resultado da raiz quadrada é o contador => 4
 
-;Se chegar em 0 é porque temos uma raiz quadrada perfeita.
+#Se chegar em 0 é porque temos uma raiz quadrada perfeita.
 
-;Porque estou utilizando 1,3,5,7 ?
+#Porque estou utilizando 1,3,5,7 ?
 
-;indice * 2 + 1
-;1º Passo => 0 * 2 + 1 => 1
-;2º Passo => 1 * 2 + 1 => 3
-;3º Passo => 2 * 2 + 1 => 5
+#indice * 2 + 1
+#1º Passo => 0 * 2 + 1 => 1
+#2º Passo => 1 * 2 + 1 => 3
+#3º Passo => 2 * 2 + 1 => 5
